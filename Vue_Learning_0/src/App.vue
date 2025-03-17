0519157ab959586fc3c9dde247867e2cb3343c24
noudas <noudas1@gmail.com>
2025-03-17 13:08:49 +0200

    Understanding inmount

--- Vue_Learning_0/src/App.vue
+++ Vue_Learning_0/src/App.vue
@@ -1,40 +1,65 @@
 <script setup>
+import { ref, onMounted } from 'vue';
 
-import { ref } from 'vue';
+const name = ref('John Doe');
+const status = ref('active');
+const tasks = ref(['Task One', 'Task Two', 'Task Three']);
+const newTask = ref('');
 
-    const name = ref('John Doe');
-    const status = ref('active');
-    const tasks = ref(['task 1', 'task 2', 'task 3']);
-    const link = ref('https://www.google.com');
+const toggleStatus = () => {
+  if (status.value === 'active') {
+    status.value = 'pending';
+  } else if (status.value === 'pending') {
+    status.value = 'inactive';
+  } else {
+    status.value = 'active';
+  }
+};
 
-    const toggleStatus = () => {
-      if (status.value === 'active') {
-        status.value = 'pending';
-      } else if (status.value === 'pending') {
-        status.value = 'inactive';
-      } else {
-        status.value = 'active';
-      }
-    };	
+const addTask = () => {
+  if (newTask.value.trim() !== '') {
+    tasks.value.push(newTask.value);
+    newTask.value = '';
+  }
+};
 
+const deleteTask = (index) => {
+  tasks.value.splice(index, 1);
+};
+
+onMounted(async () => {
+  try {
+    const response = await fetch('https://jsonplaceholder.typicode.com/todos');
+    const data = await response.json();
+    tasks.value = data.map((task) => task.title);
+  } catch (error) {
+    console.log('Error fetching tasks');
+  }
+});
 </script>
 
 <template>
-  <h1>
-    {{ name }}
-  </h1>
+  <h1>{{ name }}</h1>
   <p v-if="status === 'active'">User is active</p>
-  <p v-else-if="status === 'pending'">User is Pending</p>
+  <p v-else-if="status === 'pending'">User is pending</p>
   <p v-else>User is inactive</p>
+
+  <form @submit.prevent="addTask">
+    <label for="newTask">Add Task</label>
+    <input type="text" id="newTask" name="newTask" v-model="newTask" />
+    <button type="submit">Submit</button>
+  </form>
 
   <h3>Tasks:</h3>
   <ul>
-    <li v-for="task in tasks" :key="task"> {{ task }}</li>
+    <li v-for="(task, index) in tasks" :key="task">
+      <span>
+        {{ task }}
+      </span>
+      <button @click="deleteTask(index)">x</button>
+    </li>
   </ul>
-
-  <!-- "<a v-bind:href="link">CLick for Google</a> -->
-  <a :href="link">CLick for Google</a>
-
+  <br />
   <!-- <button v-on:click="toggleStatus">Change Status</button> -->
-  <button :click="toggleStatus">Change Status</button>
+  <button @click="toggleStatus">Change Status</button>
 </template>

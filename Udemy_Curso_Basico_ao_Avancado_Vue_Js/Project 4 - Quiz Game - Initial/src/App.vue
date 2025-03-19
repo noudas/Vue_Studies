faeb0af4d5da7c3f7694d79f656b2a30f94072b9
noudas <noudas1@gmail.com>
2025-03-19 21:58:15 +0200

    Axios in Vue

--- Udemy_Curso_Basico_ao_Avancado_Vue_Js/Project 4 - Quiz Game - Initial/src/App.vue
+++ Udemy_Curso_Basico_ao_Avancado_Vue_Js/Project 4 - Quiz Game - Initial/src/App.vue
@@ -1,47 +1,19 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+// https://opentdb.com/api.php?amount=1&type=boolean
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
+  <div>
+    <h1>This is a Question</h1>
 
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
+    <input type="radio" name="answer" value="true">
+    <label>True</label>
+    <br>
 
-  <main>
-    <TheWelcome />
-  </main>
+    <input type="radio" name="answer" value="false">
+    <label>False</label>
+    <br>
+
+    <button class="send" type="button">Send</button>
+  </div>
 </template>
-
-<style scoped>
-header {
-  line-height: 1.5;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
-</style>

6d9db4f9a52e05fadfc0cfbe3e5b3059680da33b
noudas <noudas1@gmail.com>
2025-03-19 22:13:06 +0200

    Installing and adding axios to main.js

--- Udemy_Curso_Basico_ao_Avancado_Vue_Js/Project 4 - Quiz Game - Initial/src/main.js
+++ Udemy_Curso_Basico_ao_Avancado_Vue_Js/Project 4 - Quiz Game - Initial/src/main.js
@@ -1,6 +1,11 @@
 import './assets/main.css'
 
+import * as Vue from 'vue'
+import axios from 'axios'
+import VueAxios from 'vue-axios'
 import { createApp } from 'vue'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+createApp(App)
+.mount('#app')
+.use(VueAxios, axios);

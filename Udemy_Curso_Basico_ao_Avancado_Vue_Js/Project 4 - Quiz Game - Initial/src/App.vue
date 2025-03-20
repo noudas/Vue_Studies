f47431d84c9582f2046779d02500b9c6a6bdd683
noudas <noudas1@gmail.com>
2025-03-20 12:23:40 +0200

    Using Promises on axios and vue

--- Udemy_Curso_Basico_ao_Avancado_Vue_Js/Project 4 - Quiz Game - Initial/src/App.vue
+++ Udemy_Curso_Basico_ao_Avancado_Vue_Js/Project 4 - Quiz Game - Initial/src/App.vue
@@ -1,10 +1,39 @@
-<script setup>
+<script>
+  export default {
+    name: 'App',
+
+    data(){
+      return{
+        question: undefined,
+        incorrectAnswer: undefined,
+        correctAnswer: undefined
+      }
+    },
+  
+    computed: {
+      answers(){
+        return [...this.incorrectAnswer,this.correctAnswer].sort();
+      }
+    },
+    created(){
+      this.axios
+      .get('https://opentdb.com/api.php?amount=1')
+      .then((response) =>{
+      console.log(response.data.results[0]);
+        
+        this.question = response.data.results[0].question;
+        this.incorrectAnswer = response.data.results[0].incorrect_answers;
+        this.correctAnswer = response.data.results[0].correct_answer;
+      })
+    }
+  }
+
 // https://opentdb.com/api.php?amount=1&type=boolean
 </script>
 
 <template>
   <div>
-    <h1>This is a Question</h1>
+    <h1 v-html="this.question"></h1>
 
     <input type="radio" name="answer" value="true">
     <label>True</label>
--- Udemy_Curso_Basico_ao_Avancado_Vue_Js/Project 4 - Quiz Game - Initial/src/main.js
+++ Udemy_Curso_Basico_ao_Avancado_Vue_Js/Project 4 - Quiz Game - Initial/src/main.js
@@ -7,5 +7,5 @@ import { createApp } from 'vue'
 import App from './App.vue'
 
 createApp(App)
+.use(VueAxios, axios)
 .mount('#app')
-.use(VueAxios, axios);
